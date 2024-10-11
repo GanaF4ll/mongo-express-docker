53c2e2757476cc5539b87bf55109bb9bcfd226a8
GanaF4ll <ganafall9498@gmail.com>
2024-10-11 11:39:06 +0200

    ✨ 🐛 auth-service is up

--- src/auth-service/app.ts
+++ src/auth-service/app.ts
@@ -1,8 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import userRouter from "./routes/user.route";
-// import timerRouter from "./routes/timer.route";
 
 dotenv.config();
 
@@ -11,28 +8,8 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri = process.env.MONGODB_URI as string;
 
-if (!uri) {
-  throw new Error("MONGODB_URI environment variable is not defined");
-}
-
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
-
-// app.use("/users", userRouter);
-// app.use("/timers", timerRouter);
-
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3301;
 export const server = app.listen(PORT, () => {
-  console.log(process.env.MONGODB_URI);
-
   console.log(`Auth-service is running on port ${PORT}`);
 });
