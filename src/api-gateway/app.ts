636cb515289606c2df8b65552d1d6c6f2234e70a
GanaF4ll <ganafall9498@gmail.com>
2024-10-11 11:47:37 +0200

    ⚡️ all micro-services are up

--- src/api-gateway/app.ts
+++ src/api-gateway/app.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route";
-import timerRouter from "./routes/timer.route";
+// import userRouter from "./routes/user.route";
+// import timerRouter from "./routes/timer.route";
 
 dotenv.config();
 
@@ -11,28 +10,12 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri = process.env.MONGODB_URI as string;
 
-if (!uri) {
-  throw new Error("MONGODB_URI environment variable is not defined");
-}
-
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
-
-app.use("/users", userRouter);
-app.use("/timers", timerRouter);
+// app.use("/users", userRouter);
+// app.use("/timers", timerRouter);
 
 const PORT = process.env.PORT || 3000;
 export const server = app.listen(PORT, () => {
-  console.log(process.env.MONGODB_URI);
 
   console.log(`Api-gateway is running on port ${PORT}`);
 });
